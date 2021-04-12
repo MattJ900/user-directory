98dfa000f560b456c3a07a7ef7610c9a06442469
Matthew A. John <mattj900@outlook.com>
2021-04-11 20:59:29 -0700

    Adding users Axios and adding sort function

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 import axios from"axios"
 function App() {
  const [users,setusers]=useState([]) 
+ const [name,setname] = useState("")
   useEffect(()=> {
 axios.get('https://randomuser.me/api/?results=5').then(data => {
   setusers(data.data.results)
 })
   },[])
+
   return (
     <div className="App">
     {users.map(user => (
     <div>{user?.name.first}</div> 
     ) )}  
-    
+    <button onClick={}>
+      sortbyAge
+    </button>
     </div>
   );
 }
